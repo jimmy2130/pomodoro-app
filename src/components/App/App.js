fe720c1924622214704edc5bd4f719e9bbe980b5
Chen Jimmy <jimmy@ChendeMacBook-Pro.local>
2022-09-03 23:34:32 +0800

    Project first setup

---
+++ src/components/App/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from 'styled-components/macro';
+
+function App() {
+  return (
+  	<>
+  		<Header>Hello World!!</Header>
+  	</>
+  	
+  );
+}
+
+const Header = styled.h1`
+	color: deeppink;
+`
+
+export default App;
